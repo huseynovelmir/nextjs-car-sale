9af0109ef8092d39bb98bf08d2ef98b8c3842901
huseynovelmir <huseynovelmir789@gmail.com>
2023-07-08 02:58:20 +0400

    done

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+     reactStrictMode: true,
     images : {
         domains : ["cdn.imagin.studio"]
-    }
+    },
+     eslint: {
+      ignoreDuringBuilds: true,
+    },
 }
 
 module.exports = nextConfig
